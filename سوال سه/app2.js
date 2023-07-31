dc9cc949d05538394f9109d4ce8d12704f28ff8d
arash amirsharifi <arashamirsharifi1970@gmail.com>
2023-07-31 14:05:54 +0330

    add sort function but not good

--- سوال سه/app2.js
+++ سوال سه/app2.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('.form');
+const show = document.querySelector('.show');
+const ListSort = document.querySelector('.list-user-sort')
 console.log(form)
 
 eventListener();
@@ -6,15 +8,74 @@ eventListener();
 function eventListener() {
     form.addEventListener('submit', getFrom)
 }
+
 let arrList = []
 
 function getFrom(e) {
     e.preventDefault()
-
     const listData = Object.fromEntries(new FormData(form).entries())
-
     arrList.push(listData)
-
+    // const check = Dataobj(listData)
     console.log(arrList)
     form.reset()
+    show.innerHTML = ''
+    arrList.forEach(element => {
+        const div = document.createElement('div')
+        div.classList = "list"
+        div.innerHTML = `
+     <ul class="ulList">
+     <li>
+       <p>${element.fName}</p>
+       <p>${element.age}</p>
+       </li>
+     </ul>
+     `
+        show.appendChild(div)
+    });
+
+    Sortage(arrList)
 }
+
+console.log(show)
+
+
+
+function Sortage(data) {
+
+    const sort = data.sort((a, b) => +a.age - +b.age)
+    console.log(sort)
+    sort.forEach(element => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <ul class="ulList">
+        <li>
+        <p>${element.fName}</p>
+        <p>${element.age}</p>
+        </li>
+        </ul>
+        `
+        ListSort.appendChild(div)
+
+    });
+
+};
+
+
+
+// let thebigest = userArrayObject.sort((a, b) => +b.age - +a.age);
+
+// let theBigIcome = userArrayObject.reduce((acc, item) => {
+//     return acc += +item.income
+// }, 0) / userArrayObject.length
+
+// console.log(`Average salary ::${theBigIcome}`)
+
+
+// console.log(`the oldest ${thebigest[0].myName}`)
+
+// function Dataobj(data) {
+//     console.log(data)
+//     const arr = []
+//     arr.push(data)
+//     return arr
+// }
